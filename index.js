c651a260646c003f5915c8068a67178eda1753fc
michaelleoniuk <michael.leoniuk@gmail.com>
2023-12-03 11:00:33 +0100

    Code validated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Users = Models.User;
 
 const { check, validationResult } = require('express-validator');  
 
-mongoose.connect('mongodb://localhost:27017/myFlix', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express');
   app = express();
